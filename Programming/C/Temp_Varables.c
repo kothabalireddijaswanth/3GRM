710ef69235792771773626602f0b6da066de5319
Jaswanth <k.jaswanth133@gmail.com>
2019-04-03 01:47:59 +0530

    Mofified tem varaibles code following standards

--- Programming/C/Temp_Varables.c
+++ Programming/C/Temp_Varables.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
-#include<conio.h>
 
-void main()
-    {
+int main(void)
+{
       int a,b,temp;
-            printf("Enter the a,b values");
-            scanf("%d %d",&a,&b);
+      
+      printf("Enter the a, b values : ");
+      scanf("%d %d", &a, &b);
+      
       temp=a;
       a=b;
       b=temp;
-            printf("After exchange a is %d , b is %d",a,b);
-      getch();
-    }  
+      
+      printf("\r\nAfter exchange a is %d , b is %d\r\n", a,b);
+      //getchar();
+      
+      return 0;
+}
      
     
